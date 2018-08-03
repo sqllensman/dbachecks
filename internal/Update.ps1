9725bf13571c20fa9dcbe357e5c82777983ef7bc
Patrick <sqllensman@outlook.com>
2018-08-03 14:51:54 +1000

    Changes to correct incorrect names

---
+++ internal/Update.ps1
@@ -0,0 +1,12 @@
+﻿Install-Module -name psframework
+
+Update-Module -name dbatools
+
+Update-Module -name dbachecks
+
+Install-Module -name ReportingServicesTools
+
+Install-Module -name SQLServer -AllowClobber -Force
+
+
+Install-Module -name ImportExcel -Force
---
+++ internal/scripts/Untitled1.ps1
@@ -0,0 +1,16 @@
+﻿$Credential = Get-Credential sqladmin
+
+$Credential.Username
+$Credential.Password | ConvertFrom-SecureString
+
+
+$config = Get-PSFConfig -Module dbareports
+
+$config | Out-Gridview
+
+
+Get-PSFConfigValue -FullName psframework.logging.filesystem.logpath | Invoke-Item
+
+Get-Help Stop-PSFFunction -ShowWindow
+
+Get-Help about_psf_flowcontrol -ShowWindow
